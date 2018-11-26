68fb51b0768c2a30bdfa0c2606f3a87cd7f8786c
phatsara <phatsara_ae@hotmail.com>
2018-11-26 16:20:18 +0700

    and product

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import products from './views/products.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: products
     },
     {
       path: '/about',
